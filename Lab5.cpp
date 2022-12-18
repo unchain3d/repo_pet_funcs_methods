be8cdadee031b8597f0eddbd9e4e9f27671bf55d
unchain3d <119525011+unchain3d@users.noreply.github.com>
2022-12-18 16:34:42 +0200

    Update Lab5.cpp

--- Lab5.cpp
+++ Lab5.cpp
@@ -18,12 +18,12 @@ Pet() {
     }
 
     Pet(string name, string breed, int age, string greeting, int mass, Kind petType) {
-		this->name = name;
-		this->breed = breed;
-		this->age = age;
-		this->greeting = greeting;
-		this->mass = mass;
-		this->petType = petType;
+	this->name = name;
+	this->breed = breed;
+	this->age = age;
+	this->greeting = greeting;
+	this->mass = mass;
+	this->petType = petType;
     }
 
     ~Pet() { 
@@ -31,32 +31,32 @@ Pet() {
     }
 
     string getName() {
-		return name;
+	return name;
     }
 
     string getBreed() {
-		return breed;
+	return breed;
     }
 
     int getAge() {
-		return age;
+	return age;
     }
 
     string getGreeting() {
-		return greeting;
+	return greeting;
     }
 
     int getMass() {
-		return mass;
+	return mass;
     }
 
     Kind getPetType() {
-		return petType;
+	return petType;
     }
 
     bool isPolite() {
-		return greeting.find("Hello") != string::npos;
-		// ::npos is a constant static number with the highest possible value
+	return greeting.find("Hello") != string::npos;
+	// ::npos is a constant static number with the highest possible value
     }
 
 private:
@@ -93,68 +93,68 @@ public:
     }
 
 private:
-	string Name; 
-	vector<Pet> animals;
+    string Name; 
+    vector<Pet> animals;
 };
 
 void selectionSort(vector<Pet> animals) {
-	// sorting from lowest to highest (by age)
-	int j, minIndex;
-	for (int i = 0; i < animals.size(); i++) {
-		minIndex = i;
-		for (j = i + 1; j < animals.size(); j++) {
-			if (animals[j].getAge() < animals[minIndex].getAge()) {
-				minIndex = j;
-			}
-		}
-
-		if (minIndex != i) {
-			Pet temp = animals[i];
-			animals[i] = animals[minIndex];
-			animals[minIndex] = temp;
-		}
+    // sorting from lowest to highest (by age)
+    int j, minIndex;
+    for (int i = 0; i < animals.size(); i++) {
+	minIndex = i;
+	for (j = i + 1; j < animals.size(); j++) {
+	    if (animals[j].getAge() < animals[minIndex].getAge()) {
+		minIndex = j;
+	    }
 	}
+
+	if (minIndex != i) {
+	    Pet temp = animals[i];
+	    animals[i] = animals[minIndex];
+	    animals[minIndex] = temp;
+	}
+    }
 }
 
 void printAnimals(vector<Pet> animals) {
-	for (int i = 0; i < animals.size(); i++) {
-		cout << animals[i].getAge() << " ";
-	}
-	cout << endl;
+    for (int i = 0; i < animals.size(); i++) {
+	cout << animals[i].getAge() << " ";
+    }
+    cout << endl;
 }
 
 void areFriends(vector<Pet> animals) {
-	for (int i = 0; i < animals.size(); i++) {
-		for (int j = 0; j < animals.size(); j++) {
-			int difference = animals[i].getAge() - animals[j].getAge();
-			if (i != j && difference <= 2 && difference >= 0) {
-				cout << animals[i].getName() << " and " << animals[j].getName() << endl;
-			}
-		}
+    for (int i = 0; i < animals.size(); i++) {
+        for (int j = 0; j < animals.size(); j++) {
+	    int difference = animals[i].getAge() - animals[j].getAge();
+	    if (i != j && difference <= 2 && difference >= 0) {
+		cout << animals[i].getName() << " and " << animals[j].getName() << endl;
+	    }
 	}
+    }
 }
 
 int main() {
 
-	Pet animal1 = Pet("Chunk", "x", 3, "Hello", 8, dog);
-	Pet animal2 = Pet("Cloud", "y", 5, "Meow", 3, cat);
-	Pet animal3 = Pet("David", "z", 1, "Hello, X", 3, hamster);
+    Pet animal1 = Pet("Chunk", "x", 3, "Hello", 8, dog);
+    Pet animal2 = Pet("Cloud", "y", 5, "Meow", 3, cat);
+    Pet animal3 = Pet("David", "z", 1, "Hello, X", 3, hamster);
 	
-	cout << animal1.getName() << animal1.isPolite() << endl;
-	cout << animal2.getName() << animal2.isPolite() << endl;
-	cout << animal3.getName() << animal3.isPolite() << endl;
+    cout << animal1.getName() << animal1.isPolite() << endl;
+    cout << animal2.getName() << animal2.isPolite() << endl;
+    cout << animal3.getName() << animal3.isPolite() << endl;
 
-	vector<Pet> anim = { animal1, animal2, animal3 };
+    vector<Pet> anim = { animal1, animal2, animal3 };
 	
-	// anim is the name of a dynamic array (vector)
+    // anim is the name of a dynamic array (vector)
 	
-	printAnimals(anim); 
-	selectionSort(anim); 
-	printAnimals(anim);
+    printAnimals(anim); 
+    selectionSort(anim); 
+    printAnimals(anim);
 
-	areFriends(anim);
+    areFriends(anim);
 
-	Home home = Home("My Home", anim);
+    Home home = Home("My Home", anim);
 }
 
 
